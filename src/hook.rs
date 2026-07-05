ce1b056d17006afeea68baa6b0cbe52fad7d8c64
Dasaav-dsv <simplydasaav@gmail.com>
2026-07-05 14:39:19 +0200

    (Probably) all of the `HookScope` functions?
    Signed-off-by: Dasaav-dsv <simplydasaav@gmail.com>

--- src/hook.rs
+++ src/hook.rs
@@ -5,10 +5,22 @@ pub struct HookHandle;
 #[derive(Clone, Debug)]
 pub struct HookScope<'a>(&'a HookHandle);
 
+impl HookHandle {
+    pub fn enable(&self, state: bool) {
+        todo!()
+    }
+}
+
 impl<'a> HookScope<'a> {
     /// Creates a new [`HookScope`], which immutably borrows the [`HookHandle`],
     /// but does not allow it to be cloned, forgotten or leaked.
+    #[inline]
     pub fn new(handle: &'a HookHandle) -> Self {
         Self(handle)
+    }
+
+    #[inline]
+    pub fn enable(&self, state: bool) {
+        self.0.enable(state);
     }
 }
