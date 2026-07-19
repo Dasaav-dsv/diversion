4867483185fdafcc3bbdb4ac831e1716de076098
Dasaav-dsv <simplydasaav@gmail.com>
2026-07-19 05:12:03 +0200

    Defer to `Deref<Target: HookInstaller>` instead
    Signed-off-by: Dasaav-dsv <simplydasaav@gmail.com>

--- src/installer/with.rs
+++ src/installer/with.rs
@@ -2,31 +2,21 @@ use std::ops::Deref;
 
 use crate::installer::HookInstaller;
 
-pub trait HookInstallerWithContext: HookInstaller {
-    fn with_context<Ctx>(&self, context: Ctx) -> WithContext<'_, Self, Ctx>;
-}
-
-pub struct WithContext<'a, H: ?Sized, Ctx> {
-    inner: &'a H,
+pub struct WithContext<H, Ctx> {
+    inner: H,
     context: Ctx,
 }
 
-impl<'a, H, Old, New> WithContext<'a, WithContext<'_, H, Old>, New> {
-    #[inline]
-    pub fn flatten(self) -> WithContext<'a, H, New> {
-        WithContext {
-            inner: self.inner.inner,
-            context: self.context,
-        }
-    }
+pub trait HookInstallerWithContext: Sized {
+    fn with_context<Ctx>(self, context: Ctx) -> WithContext<Self, Ctx>;
 }
 
-impl<H> HookInstallerWithContext for H
+impl<'a, H> HookInstallerWithContext for H
 where
-    H: HookInstaller,
+    H: Deref<Target: HookInstaller>,
 {
     #[inline]
-    fn with_context<Ctx>(&self, context: Ctx) -> WithContext<'_, Self, Ctx> {
+    fn with_context<Ctx>(self, context: Ctx) -> WithContext<Self, Ctx> {
         WithContext {
             inner: self,
             context,
@@ -34,12 +24,12 @@ where
     }
 }
 
-unsafe impl<H, Ctx> HookInstaller for WithContext<'_, H, Ctx>
+unsafe impl<H, Ctx> HookInstaller for WithContext<H, Ctx>
 where
-    H: HookInstaller,
+    H: Deref<Target: HookInstaller>,
     Ctx: Send + Sync + 'static,
 {
-    type Target = H::Target;
+    type Target = <H::Target as HookInstaller>::Target;
     type Context = Ctx;
 
     #[inline]
@@ -58,20 +48,31 @@ where
     }
 }
 
-impl<H, Ctx> Deref for WithContext<'_, H, Ctx> {
+impl<H, Ctx> AsRef<H> for WithContext<H, Ctx> {
+    #[inline]
+    fn as_ref(&self) -> &H {
+        &self.inner
+    }
+}
+
+impl<H, Ctx> Deref for WithContext<H, Ctx> {
     type Target = H;
 
     #[inline]
     fn deref(&self) -> &Self::Target {
-        self.inner
+        &self.inner
     }
 }
 
-impl<H, Ctx: Clone> Clone for WithContext<'_, H, Ctx> {
+impl<H, Ctx> Clone for WithContext<H, Ctx>
+where
+    H: Clone,
+    Ctx: Clone,
+{
     #[inline]
     fn clone(&self) -> Self {
         Self {
-            inner: self.inner,
+            inner: self.inner.clone(),
             context: self.context.clone(),
         }
     }
